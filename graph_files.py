7526d83f948280558d9b5d0843d914c0bd1fbed4
CaterbossIreland <ciaran.kilbride@caterboss.ie>
2025-06-27 22:42:21 +0100

    Update graph_files.py

--- graph_files.py
+++ graph_files.py
@@ -3,7 +3,7 @@ import requests
 def get_access_token():
     tenant_id = "ce280aae-ee92-41fe-ab60-66b37ebc97dd"
     client_id = "83acd574-ab02-4cfe-b28c-e38c733d9a52"
-    client_secret = "YOUR_CLIENT_SECRET_HERE"
+    client_secret = "FYX8Q~bZVXuKEenMTryxYw-ZuQOq2OBTNIu8Qa~i"
 
     url = f"https://login.microsoftonline.com/{tenant_id}/oauth2/v2.0/token"
 
