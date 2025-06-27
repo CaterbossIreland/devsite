6e47dff5df1f6f6c428a4d2d7362660b51d38ef4
CaterbossIreland <ciaran.kilbride@caterboss.ie>
2025-06-27 22:26:21 +0100

    Update graph_files.py

--- graph_files.py
+++ graph_files.py
@@ -1,13 +1,15 @@
 from graph_auth import get_access_token
 import requests
+import os
 
 GRAPH_ROOT = "https://graph.microsoft.com/v1.0"
+USER_ID = os.environ.get("USER_ID")  # 👈 You’ll add this to Render
 
 def list_onedrive_root_files():
     token = get_access_token()
     headers = {"Authorization": f"Bearer {token}"}
     
-    url = f"{GRAPH_ROOT}/me/drive/root/children"
+    url = f"{GRAPH_ROOT}/users/{USER_ID}/drive/root/children"
 
     response = requests.get(url, headers=headers)
     response.raise_for_status()
