adf1e9507dde9f1a64f71dad93e6ef871b1e8775
CaterbossIreland <ciaran.kilbride@caterboss.ie>
2025-06-27 22:38:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from graph_files import list_onedrive_root_files
 
 app = FastAPI()
 
+@app.get("/")
+def root():
+    return {"message": "Server is up and running."}
+
 @app.get("/files")
 def get_files():
-    try:
-        data = list_onedrive_root_files()
-        return data
-    except Exception as e:
-        return {"error": str(e)}
+    return list_onedrive_root_files()
